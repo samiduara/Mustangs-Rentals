d60736a02a4c43ce88524d15d2abe7c520f3e5da
sam <samgathon@hotmail.com>
2019-10-04 10:45:12 -0500

    server connects to port 3001

--- server.js
+++ server.js
@@ -16,3 +16,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
+
+  const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
