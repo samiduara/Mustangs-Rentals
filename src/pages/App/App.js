1fe29b315a7cb5d449ed150db96067e05be49ff1
sam <samgathon@hotmail.com>
2019-10-07 10:12:56 -0500

    working on authentication

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../../components/Header';
 import './App.css';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
 
-function App() {
-  return (
-    <div className="App">
-      <Header /> 
-     
-  
-     
-    </div>
-  );
-}
 
-export default App;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <LoginPage />
+        <SignupPage />
+      </div>
+    );
+  }
+ }
+ 
+
+
+ export default App;
