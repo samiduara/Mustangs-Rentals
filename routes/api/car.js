1cda39ce1a13833866689ea973557286c22a9504
sam <samgathon@hotmail.com>
2019-10-12 21:07:41 -0500

    final push

--- routes/api/car.js
+++ routes/api/car.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const carCtrl = require('../../controllers/cars');
 
+router.use(require('../../config/auth'));
+
 /*---------- Public Routes ----------*/
 
 //router.get('/cars',carCtrl.getAllCars);
@@ -9,9 +11,16 @@ const carCtrl = require('../../controllers/cars');
 
 router.get('/', carCtrl.getAllCars);
 router.post('/', carCtrl.addCar);
+router.delete('/car/:id', carCtrl.deletecar);
 
 /*---------- Protected Routes ----------*/
 
+
+
+function checkAuth(req, res, next) {
+    if (req.user) return next();
+    return res.status(401).json({msg: 'Not Authorized'});
+  }
 
 
 
